e6f63e24c3a244637591d7afc2edf2135c37dd8e
Arkhane <arkhane@okstate.edu>
2018-03-28 20:20:09 -0500

    CIS2013_Week10_Lab1
    CIS2013_Week10_Lab1

---
+++ CIS2013_Week10_Lab1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int mian()
+{
+	int v1;
+	int v2;
+	int *p1;
+	int *p2;
+
+	v1 = 22;
+	v2 = 33;
+
+	p1 = &v1;
+	p2 = &v2;
+
+	p1 = p2;
+
+	v1 = 44;
+	v2 = 55;
+
+	p1 = &v1;
+
+	cout << "p1 is: " << p1 << endl;
+	cout << "p2 is: " << p2 << endl;
+
+	
+	cout << "v1 is: " << v1 << endl;
+	cout << "v2 is: " << v2 << endl;
+
+
+	return 0;
+}
